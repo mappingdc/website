41948842967c93c2b17e15f6ed40c5124cdf6f38
Brian DeRocher <brian@derocher.org>
2015-05-30 23:48:55 -0400

    Add a slippy map of leaflet with toner tiles.
    GH-11

---
+++ js/map.js
@@ -0,0 +1,19 @@
+$(document).ready( function()
+     {
+     var map = new L.Map('map');
+ 
+     var theAttribution = 'Map tiles by [Stamen]';
+ 
+     // Change this url to use different set of map tiles
+     var theTiles = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
+     var theSubdomains = ['a', 'b', 'c', 'd'];
+     map.setView( new L.LatLng( 56, 11 ), 7 ).addLayer
+          ( new L.TileLayer
+               ( theTiles
+               ,    { maxZoom: 18
+                    , attribution: theAttribution
+                    , subdomains: theSubdomains
+                    }
+               )
+          );
+      } );
